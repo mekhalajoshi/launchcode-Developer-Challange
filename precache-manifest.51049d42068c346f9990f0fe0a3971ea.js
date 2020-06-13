128daca68cfe42655b515de01508b5aa04ec78fe
mekhalajoshi <mekhalajoshi@gmail.com>
2020-06-13 15:46:15 -0400

    Updates

---
+++ precache-manifest.51049d42068c346f9990f0fe0a3971ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d33cd05bfda5a07ed5062744e897fd1",
+    "url": "/launchcode-Developer-Challange/index.html"
+  },
+  {
+    "revision": "ff30eaab82c996e956ba",
+    "url": "/launchcode-Developer-Challange/static/css/main.0e6f50c2.chunk.css"
+  },
+  {
+    "revision": "8176452d7a5ccf887f87",
+    "url": "/launchcode-Developer-Challange/static/js/2.82638130.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/launchcode-Developer-Challange/static/js/2.82638130.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff30eaab82c996e956ba",
+    "url": "/launchcode-Developer-Challange/static/js/main.c2442060.chunk.js"
+  },
+  {
+    "revision": "6458127be0f8907713a4",
+    "url": "/launchcode-Developer-Challange/static/js/runtime-main.a51d208d.js"
+  }
+]);
