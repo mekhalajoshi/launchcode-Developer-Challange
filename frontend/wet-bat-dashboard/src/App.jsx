4daf72a64be5bcd7191412c00aa7832b65aba244
Mekhala Joshi <mekhalajoshi@users.noreply.github.com>
2020-06-14 18:04:29 -0400

    Update App.jsx

--- frontend/wet-bat-dashboard/src/App.jsx
+++ frontend/wet-bat-dashboard/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import { ThemeProvider } from '@material-ui/styles';
 
