20df2b5888f6ea49c4e662e15a2873d1c39b8929
mekhalajoshi <mekhalajoshi@gmail.com>
2020-06-14 21:28:27 -0400

    Remove mock data

--- frontend/wet-bat-dashboard/src/views/Dashboard/components/NewLeads/data.js
+++ frontend/wet-bat-dashboard/src/views/Dashboard/components/NewLeads/data.js
@@ -5,36 +5,24 @@ export default [
     id: uuid(),
     name: 'Jane Smith',
     imageUrl: '/images/avatars/avatar_1.png',
-    msg: 'Hey! I want to pace my package'
+    msg: 'Hey! I want to pace my package',
   },
   {
     id: uuid(),
     name: 'Jane Smith',
     imageUrl: '/images/avatars/avatar_2.png',
-    msg: 'Hey! I want to pace my package'
+    msg: 'Hey! I want to pace my package',
   },
   {
     id: uuid(),
     name: 'Jane Smith',
     imageUrl: '/images/avatars/avatar_3.png',
-    msg: 'Hey! I want to pace my package'
+    msg: 'Hey! I want to pace my package',
   },
   {
     id: uuid(),
     name: 'Jane Smith',
     imageUrl: '/images/avatars/avatar_4.png',
-    msg: 'Hey! I want to pace my package'
+    msg: 'Hey! I want to pace my package',
   },
-  {
-    id: uuid(),
-    name: 'Jane Smith',
-    imageUrl: '/images/avatars/avatar_5.png',
-    msg: 'Hey! I want to pace my package'
-  },
-  {
-    id: uuid(),
-    name: 'Jane Smith',
-    imageUrl: '/images/avatars/avatar_6.png',
-    msg: 'Hey! I want to pace my package'
-  }
 ];
